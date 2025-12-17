7370346cb2f7d5c4c58fab10b7e6444181440fd1
Andrew Rowatt <a.j.rowatt@massey.ac.nz>
2025-12-17 14:44:57 +1300

    Code tidy-ups

--- tests/player_test.php
+++ tests/player_test.php
@@ -79,7 +79,6 @@ final class player_test extends \advanced_testcase {
             $CFG->media_default_height . '"~', $content);
         $this->assertMatchesRegularExpression('~Presentation is currently private~', $content);
 
-
         $url = new \moodle_url('https://webcast.massey.ac.nz/Mediasite/Play/fb1b6a3187754c17af1b399e734a22b51d');
         $text = \html_writer::link($url, 'Lorem ipsum dolor sit amet');
         $content = format_text($text, FORMAT_HTML);
